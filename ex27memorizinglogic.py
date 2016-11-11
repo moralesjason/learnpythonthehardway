690b200c7c4c40943d9ad44766816b37a96bbff5
Jason Morales <jasonmorales@deltek.com>
2016-11-12 06:27:42 +0800

    up to ex33

---
+++ ex27memorizinglogic.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+print "Memorizing Logic"
+
+print "----------The Truth Terms----------"
+
+print "and"
+
+print "or"
+
+print "not"
+
+print "!= (not equal)"
+
+print "== (equal)"
+
+print ">= (greater-than-equal)"
+
+print "<= (less-than-equal)"
+
+print "True"
+
+print "False"
+
+print ""
+
+print "----------The Truth Tables----------"
+
+print "NOT       |True?"
+print "not False |True"
+print "not True  |False"
+
+print ""
+
+print "OR            |True?"
+print "True or False |True"
+print "True or True  |True"
+print "False or True |True"
+print "False or False|False"
+
+print ""
+
+print "AND            |True?"
+print "True and False |False"
+print "True and True  |True"
+print "False and True |False"
+print "False and False|False"
+
+print ""
+
+print "NOT OR              |True?"
+print "not (True or False) |False"
+print "not (True or True)  |False"
+print "not (False or True) |False"
+print "not (False or False)|True"
+
+print ""
+
+print "NOT AND              |True?"
+print "not (True and False) |True"
+print "not (True and True)  |False"
+print "not (False and True) |True"
+print "not (False and False)|True"
+
+print ""
+
+print "!=     |True?"
+print "1 != 0 |True"
+print "1 != 1 |False"
+print "0 != 1 |True"
+print "0 != 0 |False"
+
+print ""
+
+print "==     |True?"
+print "1 == 0 |False"
+print "1 == 1 |True"
+print "0 == 1 |False"
+print "0 == 0 |True"
