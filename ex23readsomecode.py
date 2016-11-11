0ed3beaf7ed453f27db6cb7b2c373c86f190db4f
Jason Morales <jasonmorales@deltek.com>
2016-11-12 01:57:46 +0800

    up to ex25

---
+++ ex23readsomecode.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+print "No coding for exercise 23"
+print "only reading codes on github etc."
---
+++ ex25output.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import ex25evenmorepractice
+
+sentence = "All good things come to those who wait."
+
+words = ex25evenmorepractice.break_words(sentence)
+
+print words
