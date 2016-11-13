401de0cfcdaa6acd7aa7c2f5a88f55b4c8141bcf
Jason Morales <jasonmorales@deltek.com>
2016-11-13 08:29:05 +0800

    up to ex39

---
+++ ex36designinganddebugging.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+
+print "----------RULES for IF-STATEMENTS----------"
+
+print "Every if-statement must have an else."
+
+print "If this else should never run beacuse it doesn't make sense, then you must use a die function in the else that prints out an error message and dies, just like we did in the last exercise. This will find many errors."
+
+print "Never nest if-statements more than two deep and always try to do them one deep."
+
+print "Treat if-statemetnts like paragraphs, where each if-elif-else grouping is like a set of sentences. Put blank lines before and after."
+
+print "Your boolean tests should be simple. If they are complex, move their calculations to variables earlier in your function and use a good name for the variable."
+
+print ""
+
+print "----------RULED for LOOPS----------"
+
+print "Use a while-loop only to loop forever, and that means probably never. This only applies to Python;other languages are different."
+
+print "Use a for-loop for all other kinds of looping, especially if there is a fixed or limited number of things to loop over."
+
+print ""
+
+print "----------TIPS for DEBUGGING----------"
+
+print 'Do not use a "debugger". A debugger is like doing a full-body scan on a sick person. You do not get specific useful information, and you find a whole lot of information that doesn\'t help and is just confusing.'
+
+print "The best way to debug a program is to use print to print out the values of variables at points in the program to see where they go wrong."
+
+print "Make sure parts of your program work as you work on them. Do not write massive files of code before you try to run them. Code a little, run a little,fix a little." 
+
