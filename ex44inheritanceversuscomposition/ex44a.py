7eb318cf7a456ef922e64e8dd3ecc475d02ca88a
Jason Morales <jasonmorales@deltek.com>
2016-11-16 01:11:16 +0800

    ex44

---
+++ ex44inheritanceversuscomposition/ex44a.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+class Parent(object):
+
+	def implicit(self):
+		print "PARENT implicit()"
+
+class Child(Parent):
+	pass
+
+dad = Parent()
+son = Child()
+
+dad.implicit()
+son.implicit()
